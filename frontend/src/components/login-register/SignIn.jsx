fe041b7f78a4db533a4b43f0857bd3a5bf680f30
nonaghazizadeh <nonaaghazizadeh@gmail.com>
2022-01-23 01:18:50 +0330

    feat: change to functional react and handle inputs in sign in

--- frontend/src/components/login-register/SignIn.jsx
+++ frontend/src/components/login-register/SignIn.jsx
@@ -1,21 +1,36 @@
 import React from "react";
 
-class SignIn extends React.Component {
-  render() {
-    return (
-      <div className="signInContainer">
-        <h4 className="headerText">Welcome Back</h4>
-        <div className="inputSection">
-          <input type="text" className="userName login" required />
-          <label className="inputLabel">User Name</label>
-        </div>
-        <div className="inputSection">
-          <input type="text" className="password login" required />
-          <label className="inputLabel">Password</label>
-        </div>
+const SignIn = ({usernameSignIn, passwordSignIn, setUserNameSignIn, setPasswordSignIn}) => {
+  return (
+    <div className="signInContainer">
+      <h4 className="headerText">Welcome Back</h4>
+      <div className="inputSection">
+        <input 
+          type="text" 
+          className="userName login" 
+          value={usernameSignIn} 
+          required 
+          onChange={(e) => {
+            setUserNameSignIn(e.target.value)
+          }}
+        />
+        <label className="inputLabel">User Name</label>
       </div>
-    );
-  }
+      <div className="inputSection">
+        <input 
+          type="text" 
+          className="password login" 
+          value={passwordSignIn} 
+          required 
+          onChange={(e) => {
+            setPasswordSignIn(e.target.value)
+        }}
+
+        />
+        <label className="inputLabel">Password</label>
+      </div>
+    </div>
+  );
 }
 
 export default SignIn;

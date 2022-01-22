39347b4ab54204585ce448fe8c73c8a8648a42ae
ShayanEmzed <shayan80sama@gmail.com>
2022-01-22 23:53:47 +0330

    chore: read ports from .env

--- backend/app/config/cache-cli.js
+++ backend/app/config/cache-cli.js
@@ -3,10 +3,10 @@ import protoLoader from '@grpc/proto-loader';
 import dotenv from 'dotenv';
 import { resolve, dirname } from 'path';
 import { fileURLToPath } from 'url';
-dotenv.config()
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const PROTO_PATH = resolve(__dirname, '../../../cache/lru.proto');
+dotenv.config({path: resolve(__dirname, '../.env')});
 
 const options = {
     keepCase: true,
@@ -15,6 +15,6 @@ const options = {
 var packageDefinition = protoLoader.loadSync(PROTO_PATH,options);
 const CacheService = grpc.loadPackageDefinition(packageDefinition).CacheService;
 
-let port = process.env.PORT || 8081;
+let port = process.env.CACHE_PORT || 8081;
 const cache_cli = new CacheService(`localhost:${port}`, grpc.credentials.createInsecure())
 export default cache_cli
--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ db.sequelize.sync();
 set_note_router(app);
 set_user_router(app);
 
-const PORT = process.env.PORT;
+const PORT = process.env.BACKEND_PORT;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });

73bbf3ef6cd0710d8b10b89999c5c1f674c90cbc
nonaghazizadeh <nonaaghazizadeh@gmail.com>
2022-01-23 01:19:37 +0330

    feat: add functional react and handle inputs in sign up

--- frontend/src/components/login-register/SignUp.jsx
+++ frontend/src/components/login-register/SignUp.jsx
@@ -1,20 +1,35 @@
 import React from "react";
 
-class SignUp extends React.Component {
-  render() {
-    return (
-      <div className="signUpContainer">
-        <h4 className="headerText">Join Us Today</h4>
-        <div className="inputSection">
-          <input type="text" className="userName login" required />
-          <label className="inputLabel">User Name</label>
-        </div>
-        <div className="inputSection">
-          <input type="password" className="password login" required />
-          <label className="inputLabel">Password</label>
-        </div>
+const SignUp = ({usernameSignUp, passwordSignUp, setUserNameSignUp, setPasswordSignUp}) => {
+  return (
+    <div className="signUpContainer">
+      <h4 className="headerText">Join Us Today</h4>
+      <div className="inputSection">
+        <input 
+        type="text" 
+        className="userName login" 
+        value={usernameSignUp} 
+        required 
+        onChange={(e) => {
+          setUserNameSignUp(e.target.value)
+        }}
+        />
+        <label className="inputLabel">User Name</label>
       </div>
-    );
-  }
+      <div className="inputSection">
+        <input 
+        type="password" 
+        className="password login" 
+        value={passwordSignUp} 
+        required 
+        onChange={(e) => {
+          setPasswordSignUp(e.target.value)
+        }}
+        />
+        <label className="inputLabel">Password</label>
+      </div>
+    </div>
+  );
 }
+
 export default SignUp;
